cf2e944eec23a5705d1e77ece35de5387869183b
brianmiro <brian771@hotmail.com>
2016-08-19 00:15:14 -0300

    Update Cls_Persona.cs
    gets & sets

--- ActividadVirtual2/Cls_Persona.cs
+++ ActividadVirtual2/Cls_Persona.cs
@@ -10,13 +10,58 @@ using System;
 
 namespace ActividadVirtual2
 {
-	/// <summary>
-	/// Description of Cls_Persona.
-	/// </summary>
+	
 	public class Cls_Persona
 	{
 		public Cls_Persona()
 		{
 		}
-	}
-}
+	public Cls_Persona(string nom,string apell,long dni,int edad,int tel)//Constructor 2
+		{
+			this.nombre=nom;
+			this.apellido=apell;
+			this.dni=dni;
+			this.edad=edad;
+			this.tel=tel;
+		}
+		//Metodos
+		public void setNombre(string nom)
+		{
+			this.nombre=nom;
+		}
+		public void setApellido(string a)
+		{
+			this.apellido=a;
+		}
+		public void setDni(long dni)
+		{
+			this.dni=dni;
+		}
+		public void setEdad(int e)
+		{
+			this.edad=e;
+		}
+		public void setTel(int t)
+		{
+			this.tel=t;
+		}
+		public string getNombre()
+		{
+			return this.nombre;
+		}
+		public string getApellido()
+		{
+			return this.apellido;
+		}
+		public long getDni()
+		{
+			return this.dni;
+		}
+		public int getEdad()
+		{
+			return this.edad;
+		}
+		public  int getTel()
+		{
+			return this.tel;
+		}
