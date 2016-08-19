6ef9b1af46e0c5a775475f39fd5268020e83f68a
gmogro <guillermomogro51@gmail.com>
2016-08-19 00:22:54 -0300

    Update Cls_MiCuenta.cs

--- ActividadVirtual2/Cls_MiCuenta.cs
+++ ActividadVirtual2/Cls_MiCuenta.cs
@@ -10,51 +10,27 @@ namespace ActividadVirtual2
 	public class Cls_MiCuenta
 	{
 		//Atributos
-		private int NroCuenta=0,saldo=0;
+		private int NroCuenta=0;
+		private double saldo=0;
 		private string Propietario,FechaAlta;
 	
 		public Cls_MiCuenta()//Constructor 1
 		{
 		}
-		public Cls_MiCuenta(int nroC,int s,string prop,string fechA)//Constructor 2
+		public Cls_MiCuenta(int nroC,int s,string prop,string fechA)
 		{
-			this.NroCuenta=nroC;
-			this.saldo=s;
-			this.Propietario=prop;
-			this.FechaAlta=fechA;
+	
 		}
-		public void setNroCuenta(int NroC)
-		{
-			this.NroCuenta=NroC;
-			
-		}
-		public void setSaldo(int s)
-		{
-			this.saldo=s;
-		}
-		public void setProp(string p)
-		{
-			this.Propietario=p;
-		}
-		public void setFechaA(string f)
-		{
-			this.FechaAlta=f;
-		}
-		public int getNroCuenta()
-		{
-			return this.NroCuenta;
-		}
-		public int getSaldo()
-		{
-			return this.saldo;
-		}
-		public string getProp()
-		{
-			return this.Propietario;
-		}
-		public string getFechA()
-		{
-			return this.FechaAlta;
+		public Boolean transaccion(double cantidad,string tipo){
+			if(String.equals(tipo,"deposito"))
+			{
+				saldo=saldo+cantidad;
+			}
+			else if(saldo>cantidad)
+				{
+					saldo=saldo-cantidad;
+				}
+			     else saldo=0;
 		}
 	}
 }
